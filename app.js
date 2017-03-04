531b9d1c87451eabed79fb2c96744161d866d270
carlogropif <carlchampain@gmail.com>
2017-03-04 13:46:08 -0800

    implemented counter

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 
+var counter1 = 0;
+var counter2 = 0;
+
+
 $(document).ready(function() {
-  console.log("Hey, app.js is connected!");
+    console.log("Hey, app.js is connected!");
 
 
-           while (counter < 15) {
+    $('body').keydown(function(e) {
+        e.preventDefault();
+        console.log('you pressed a key');
 
-           var counter1 = 0;
-           var counter2 = 0;
+        if (counter1 < 21) {
 
-            $('body').keydown(function(e) {
-              e.preventDefault();
-              console.log('you pressed a key');
-                if (e.which === 39) {
-                  // when a key is pressed, add an increment to a counter, and pass into the css method below AND use that counter to track if win
-                  $('#img1').css({'margin-left': '+=50' });
-                  counter1++;
-                }
-                if (e.which === 32){
-                  $('#img2').css({'margin-left': '+=50' });
-                  counter2++;
-                }
-            });
-          }
+            if (e.which === 39) {
+                // when a key is pressed, add an increment to a counter, and pass into the css method below AND use that counter to track if win
+                $('#img1').css({
+                    'margin-left': '+=50'
+                });
+                counter1++;            }
+        } // end if
 
+        if(counter2 < 21){
 
-});
+            if (e.which === 32) {
+                $('#img2').css({
+                    'margin-left': '+=50'
+                });
+                counter2++;
+            }
+        } // end if
 
-
-
+    }); // end keydown
 
 
-            // Keep track of the car click
-            // while loop to end the race
+
+}); // end document ready
+
+
+
+
+
+
+// Keep track of the car click
+// while loop to end the race      counter 21
+
+
+//boolean = hasWon    -include a reset button on the screen or in the hasWon
+//prompt user for their name
+//display message when  one of the user wins
+// countdown timer at the start
+//display message halfway ('ALmost there')
