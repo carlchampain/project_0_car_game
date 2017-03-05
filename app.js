954c6b2c96433f4bbf5d5de909a7ed631fc6451e
carlogropif <carlchampain@gmail.com>
2017-03-04 17:00:32 -0800

    determine a winner and added reset button

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
-
-var counter1 = 0;
-var counter2 = 0;
+var player1 = 0;
+var player2 = 0;
 
 
 $(document).ready(function() {
+
     console.log("Hey, app.js is connected!");
 
+    var hasWon = false;
 
     $('body').keydown(function(e) {
         e.preventDefault();
         console.log('you pressed a key');
 
-        if (counter1 < 21) {
+        if (!hasWon) {
 
-            if (e.which === 39) {
-                // when a key is pressed, add an increment to a counter, and pass into the css method below AND use that counter to track if win
-                $('#img1').css({
-                    'margin-left': '+=50'
-                });
-                counter1++;            }
-        } // end if
+            if (player1 < 21 && !hasWon) {
 
-        if(counter2 < 21){
-
-            if (e.which === 32) {
-                $('#img2').css({
-                    'margin-left': '+=50'
-                });
-                counter2++;
+                if (e.which === 39) {
+                    // when a key is pressed, add an increment to a counter, and pass into the css method below AND use that counter to track if win
+                    $('#img1').css({
+                        'margin-left': '+=50'
+                    });
+                    player1++;
+                }
             }
-        } // end if
+
+            if (player2 < 21 && !hasWon) {
+
+                if (e.which === 32) {
+                    $('#img2').css({
+                        'margin-left': '+=50'
+                    });
+                    player2++;
+                }
+            }
+
+            if (player1 === 21) {
+                $('.winner').append('Player 1 has won the game!');
+                hasWon = true;
+            }
+            if (player2 === 21) {
+                $('.winner').append('Player 2 has won the game!');
+                hasWon = true;
+            }
+        }
 
     }); // end keydown
 
-
+     $('#button').click(function(){
+       window.location.reload();
+     });
 
 }); // end document ready
 
@@ -42,12 +58,6 @@ $(document).ready(function() {
 
 
 
-// Keep track of the car click
-// while loop to end the race      counter 21
-
-
-//boolean = hasWon    -include a reset button on the screen or in the hasWon
 //prompt user for their name
-//display message when  one of the user wins
 // countdown timer at the start
 //display message halfway ('ALmost there')
